cc606d6b1de16cd13ed7755cf733ec221f0dcd57
nurbolatk <nurbolattk@gmail.com>
2021-12-14 22:12:05 +0600

    create logo

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-import "./style.css";
+import "./assets/styles/variables.css";
+import "./assets/styles/index.css";
+
+const menuOpener = document.getElementById("menu-opener");
+const menuCloser = document.getElementById("menu-closer");
+const mobileMenu = document.getElementById("mobile-menu");
+
+menuOpener?.addEventListener("click", () => {
+  mobileMenu?.classList.remove("hidden");
+});
+menuCloser?.addEventListener("click", () => {
+  mobileMenu?.classList.add("hidden");
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      raleway: ["Raleway", "system-ui", "sans-serif"],
+      roboto: ["Roboto", "system-ui", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        accent: "#f88500",
+      },
+      textColor: {
+        black: "#333333",
+        white: "#999999",
+      },
+    },
   },
   plugins: [],
 };
