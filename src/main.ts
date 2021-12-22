4d50947f0563a35d43d14514cac6712615f0a707
nurbolatk <nurbolattk@gmail.com>
2021-12-22 14:57:58 +0600

    create modal

--- src/main.ts
+++ src/main.ts
@@ -12,3 +12,38 @@ menuOpener?.addEventListener("click", () => {
 menuCloser?.addEventListener("click", () => {
   mobileMenu?.classList.add("hidden");
 });
+
+const sendRequestButton = document.getElementById("send-request");
+const modalOverlay = document.querySelector(
+  ".modal-overlay"
+) as HTMLDivElement | null;
+const closeModalButton = document.getElementById("close-modal");
+
+sendRequestButton?.addEventListener("click", () => {
+  modalOverlay?.classList.add("open");
+});
+
+modalOverlay?.addEventListener("click", (e: MouseEvent) => {
+  const { target } = e;
+  if (target instanceof Element) {
+    const isOutside = target.closest(".modal-card") === null;
+    if (isOutside) {
+      modalOverlay?.classList.remove("open");
+    }
+  }
+});
+
+closeModalButton?.addEventListener("click", (e) => {
+  const { target } = e;
+  if (target instanceof Element) {
+    modalOverlay?.classList.remove("open");
+  }
+});
+
+document.addEventListener("keyup", (e) => {
+  if (e.key === "Escape") {
+    if (modalOverlay?.classList.contains("open")) {
+      modalOverlay?.classList.remove("open");
+    }
+  }
+});
